714da8b9768e7925aba3d5cb16887da46b880230
Rodrigo Callisaya <rodrigoliquid@gmail.com>
2022-02-07 20:59:52 -0400

    ADICION PLUGIN maven-assembly-plugin

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,18 +1,28 @@
 package com.example;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+
 /**
  * Hello world!
  */
 public final class App {
+
+    /**
+    * Frase corta descriptiva.
+    * Descripción de la clase.
+    * @author Rodrigo Callisaya
+    * @version 1.0, 07/02/2022
+    */
+
+    public static final int PUERTO = 3456;
+
     private App() {
     }
 
@@ -25,7 +35,7 @@ public final class App {
     public static void main(String[] args) throws Exception {
         Properties properties = new Properties();
         properties.load(new FileInputStream(new File("src/main/resources/vars.properties")));
-        port(3456);
-        get("/entornos", (req, res) -> "En el ambiente " + properties.getProperty("AMBIENTE_PRU"));
+        port(PUERTO);
+        get("/entornos", (req, res) -> "En el ambiente " + properties.getProperty("AMBIENTE"));
     }
 }
